153a6ddcaf7f1de35ce2cedf839db2065fe48f51
Ori Markowitz <ori.markowitz@gmail.com>
2020-11-22 20:59:39 -0500

    finished refactoring with modules lab

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,7 +6,7 @@
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 
 
-require_relative '../config/environment'
+require_relative '../config/environment.rb'
 
 
 RSpec.configure do |config|
